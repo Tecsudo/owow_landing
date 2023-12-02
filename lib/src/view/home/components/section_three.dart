21cb87940be31c5e62e885fe194ee9c6a5ab4259
muhammadhaad <haad1111997@gmail.com>
2023-12-02 13:18:35 +0500

    Remove unnecessary code in SectionThree

--- lib/src/view/home/components/section_three.dart
+++ lib/src/view/home/components/section_three.dart
@@ -33,43 +33,6 @@ class _SectionThreeState extends State<SectionThree> {
               ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: screenSize.width * .01),
-            width: screenSize.width,
-            height: screenSize.height * 1.5,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Spacer(),
-                SizedBox(
-                  height: screenSize.height * 1.5,
-                  child: Flexible(
-                    child: Column(
-                      children: [
-                        EndContentShapes(screenSize: screenSize),
-                        EndContentShapes(screenSize: screenSize),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: screenSize.height * 1.5,
-                  child: Flexible(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: screenSize.height * .25,
-                        ),
-                        EndContentShapes(screenSize: screenSize),
-                        EndContentShapes(screenSize: screenSize),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: Padding(
@@ -130,38 +93,6 @@ class _SectionThreeState extends State<SectionThree> {
             ),
           ),
           const Align(alignment: Alignment.bottomCenter, child: Footer()),
-        ],
-      ),
-    );
-  }
-}
-
-class EndContentShapes extends StatelessWidget {
-  const EndContentShapes({super.key, required this.screenSize});
-  final Size screenSize;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      width: screenSize.width * .25,
-      height: screenSize.height * .6,
-      decoration: ShapeDecoration(
-        gradient: const LinearGradient(
-          begin: Alignment(0.71, 0.70),
-          end: Alignment(-0.71, -0.7),
-          colors: [Color(0xFF4074BA), Color(0xFF5EA3D9)],
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(51),
-        ),
-        shadows: const [
-          BoxShadow(
-            color: Color(0x33000000),
-            blurRadius: 41.50,
-            offset: Offset(0, 0),
-            spreadRadius: 6,
-          )
         ],
       ),
     );
