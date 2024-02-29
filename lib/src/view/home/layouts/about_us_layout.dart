7dd0cba340f00f47ded44143b9793e4baf84e3a0
muhammadhaad <haad1111997@gmail.com>
2024-02-29 16:21:59 +0500

    Update dependencies and imports

--- lib/src/view/home/layouts/about_us_layout.dart
+++ lib/src/view/home/layouts/about_us_layout.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:owow/core/constant/gap_constant.dart';
 import 'package:owow/core/extensions/build_context_extension.dart';
 import 'package:owow/core/extensions/responsive_framwork.dart';
-
-import 'package:responsive_framework/responsive_row_column.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../../../../../core/constant/size_constant.dart';
 import '../../../../../core/constant/ui_constant.dart';
